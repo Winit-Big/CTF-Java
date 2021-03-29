0cbb6ea88d2187be1a0cb818143f7b5e866cd25b
Winit Sangprateeptip <winit.sangprateeptip@gmail.com>
2021-03-29 09:01:49 +0700

    Add files via upload

---
+++ Java_Part2/6/src/com/winitsa/Lesson/Lesson.java
@@ -0,0 +1,9 @@
+package com.winitsa.Lesson;
+
+public class Lesson {
+	public static void main(String [] args) {
+		char[] arrChar = {'C','T','F','0','1', '0', '9', '2', '9'};
+		String strWord = new String(arrChar);
+		System.out.println(strWord);
+	}
+}
